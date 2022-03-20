3789dd67bdd13c7c61a1f4c175dbc10988c9cf9d
quangtho908 <quangtho23062002@gmail.com>
2022-03-20 20:36:37 +0700

    error page

---
+++ src/common/exception/http.exception.ts
@@ -0,0 +1,14 @@
+import { ArgumentsHost, Catch, ExceptionFilter, HttpException } from "@nestjs/common";
+import { Request, Response } from "express";
+
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter {
+    catch(exception: HttpException, host: ArgumentsHost) {
+        const ctx = host.switchToHttp();
+        const response = ctx.getResponse<Response>();
+        const status = exception.getStatus();
+        const error = exception.message
+
+        response.render('pageError/index', {title: status, description: "Page not found", message: error})
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './common/exception/http.exception';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -9,6 +10,8 @@ async function bootstrap() {
   app.setBaseViewsDir(join(__dirname, '..', 'client/views'));
   app.useStaticAssets(join(__dirname, '..', 'client/public'));
   app.setViewEngine('ejs');
+
+  app.useGlobalFilters(new HttpExceptionFilter())
 
   await app.listen(3000);
 }
