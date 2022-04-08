2208c98a9c8d6963dc93a8994154ce7be75bcdc8
quangtho908 <quangtho23062002@gmail.com>
2022-04-08 11:30:07 +0700

    test

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 /* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { AboutUsModule } from './aboutUs/aboutUs.module';
 import { ContactModule } from './contact/contact.module';
 import { CustomerModule } from './customer/ customer.module';
@@ -8,6 +8,7 @@ import { DepartmentModule } from './department/department.module';
 import { HomeModule } from './home/home.module';
 import { PersonnelModule } from './personnel/personnel.module';
 import { ProductModule } from './product/product.module';
+
 @Module({
   imports: [
     HomeModule,
